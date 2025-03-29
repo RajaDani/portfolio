0f44e6878e7956ea098239d4bb4c06e55cd36165
Danish <danishimran889@gmail.com>
2025-03-30 00:58:12 +0500

    home changed

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,12 +6,14 @@ import About from '../about/page'
 import Skills from '../skills/page'
 import FeaturedProjects from '../projects/page'
 import Contact from '../contact/page'
+import Services from '../services/page'
 
 export default function Home() {
     return (
         <div className='bg-gray-50'>
             <Navbar />
             <About />
+            <Services />
             <Skills />
             <FeaturedProjects />
             <Contact />
