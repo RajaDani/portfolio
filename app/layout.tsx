2b0e57aaa33a3d113d7a99040bdbd108d89d9513
Danish <danishimran889@gmail.com>
2025-03-30 01:01:01 +0500

    home changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${roboto.className} antialiased`}
+        className={`${roboto.className} antialiased overflow-x-hidden`}
       >
         <Navbar />
         <AntdRegistry>{children}</AntdRegistry>
