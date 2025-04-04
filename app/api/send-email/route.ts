4820b90b40d545813359932064368c6d017e6da2
Danish <danishimran889@gmail.com>
2025-04-04 20:20:22 +0500

    email req changed

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -30,9 +30,9 @@ export async function POST(req: NextRequest) {
             },
         )
 
-        return NextResponse.json({ message: "Email sent successfully" }, { status: 200 });
+        // return NextResponse.json({ message: "Email sent successfully" }, { status: 200 });
     } catch (error) {
         console.error("Error sending email:", error);
-        return NextResponse.json({ message: "Failed to send email" }, { status: 500 });
+        // return NextResponse.json({ message: "Failed to send email" }, { status: 500 });
     }
 }
